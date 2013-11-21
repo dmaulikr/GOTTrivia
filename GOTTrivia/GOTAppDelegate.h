ebc4caa5f3e36f0ad43aba5fd8c15223a19a28f4
James Hicklin <hickin.james@gmail.com>
2013-11-21 08:42:53 -0800

    Initial Commit

---
+++ GOTTrivia/GOTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GOTAppDelegate.h
+//  GOTTrivia
+//
+//  Created by James Hicklin on 11/21/2013.
+//  Copyright (c) 2013 James Hicklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GOTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GOTTrivia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GOTTrivia
+//
+//  Created by James Hicklin on 11/21/2013.
+//  Copyright (c) 2013 James Hicklin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GOTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([GOTAppDelegate class]));
+  }
+}
